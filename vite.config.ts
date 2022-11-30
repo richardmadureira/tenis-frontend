6c387a7fd2980cbe3cededae84656b5c19dc7456
Richard Mendes Madureira <richard.madureira@dataprev.gov.br>
2022-11-30 14:32:48 -0300

    teste github action

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-
-  base: 'https://richardmadureira.github.io/tenis-frontend/',
+  plugins: [
+    react(),
+    splitVendorChunkPlugin()
+  ],
+  base: "/tenis-frontend/"
 })
