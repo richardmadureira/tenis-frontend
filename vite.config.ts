00b495cc4325e8751d47448980973a97dc4650d6
Richard Mendes Madureira <richard.madureira@dataprev.gov.br>
2022-11-30 14:00:41 -0300

    inclusão de homepage

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
+  base: 'https://richardmadureira.github.io/tenis-frontend/',
   server: {
     host: '0.0.0.0'
   }
